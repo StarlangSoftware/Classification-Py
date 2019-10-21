6f4a2c99e6ba1704d46fae98091b5319f9a365f7
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-10-21 22:56:11 +0300

    Updated Classification repository translation

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-Classification',
+    version='1.0.0',
+    packages=['Classification', 'Classification.Model', 'Classification.Model.DecisionTree', 'Classification.Filter',
+              'Classification.DataSet', 'Classification.Instance', 'Classification.Attribute',
+              'Classification.Parameter', 'Classification.Classifier', 'Classification.Experiment',
+              'Classification.Performance', 'Classification.InstanceList', 'Classification.DistanceMetric',
+              'Classification.StatisticalTest', 'Classification.FeatureSelection'],
+    url='https://github.com/olcaytaner/Classification-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Classification library'
+)
