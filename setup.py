31e7f42b30f30442da38fc8abdabb082a3bd54da
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:38:39 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Classification',
-    version='1.0.10',
+    version='1.0.11',
     packages=['Classification', 'Classification.Model', 'Classification.Model.DecisionTree', 'Classification.Filter',
               'Classification.DataSet', 'Classification.Instance', 'Classification.Attribute',
               'Classification.Parameter', 'Classification.Classifier', 'Classification.Experiment',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Classification library',
-    install_requires=['NlpToolkit-Math', 'NlpToolkit-DataStructure', 'NlpToolkit-Sampling']
+    install_requires=['NlpToolkit-Math', 'NlpToolkit-DataStructure', 'NlpToolkit-Sampling'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
